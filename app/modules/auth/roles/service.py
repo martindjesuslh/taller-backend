b8578c25a9d8077e4ee7e6f1a8983ed069624d25
Martin De Jesus <martindjesuslh@gmail.com>
2025-12-11 19:41:59 -0600

    added methos get, post and put for intertaction with roles

--- app/modules/auth/roles/service.py
+++ app/modules/auth/roles/service.py
@@ -1,27 +1,49 @@
-from typing import List, Dict, Any, Optional
-from logging import getLogger
-from fastapi import HTTPException,Response
+from typing import Optional
 
 from app.database.query_manager import QueryManager, QueryResult
+from app.database.query_builder import QueryBuilder
 from app.schemas.responses import ApiResponse, from_status
-
-logger = getLogger(__name__)
 
 
 class RoleService:
     def __init__(self):
         self.qm = QueryManager()
 
-    async def create(
-        self, name: str, description: Optional[str], is_active: bool
-    ) -> QueryResult:
+    async def check_name(self, name):
         query = """
-            INSERT INTO "user".roles (name, description, is_active)
-            VALUES ($1, $2, $3)
-            RETURNING id, name, description, is_active
+            SELECT 1 FROM "user".roles WHERE name = $1 LIMIT 1;
         """
-        params = (name, description, is_active)
+        params = (name,)
 
+        response = await self.qm.select(query, params)
+        exists_register = bool(response.data)
+
+        return (
+            ApiResponse.no_content()
+            if exists_register
+            else ApiResponse.not_found("Role not found")
+        )
+
+    async def get(self, name, active):
+        qb = QueryBuilder("user", "roles")
+        qb.select().where(name=name, is_active=active)
+
+        query, params = qb.build_select()
+
+        response = await self.qm.select(query, params)
+        exists_register = bool(response.data)
+
+        return (
+            ApiResponse.ok(data=response.data)
+            if exists_register
+            else ApiResponse.no_content("Not found")
+        )
+
+    async def create(self, name, description, is_active) -> QueryResult:
+        qb = QueryBuilder("user", "roles")
+        qb.insert(name=name, description=description, is_active=is_active)
+
+        query, params = qb.build_insert(["id", "name", "description", "is_active"])
         response = await self.qm.write(query, params, True)
 
         if not response.success:
@@ -30,3 +52,19 @@ class RoleService:
             )
 
         return ApiResponse.created("Role created successfully", response.data)
+
+    async def update(self, id, name, description, is_active):
+        qb = QueryBuilder("user", "roles")
+        qb.set(name=name, description=description, is_active=is_active)
+        qb.where(id=id)
+
+        query, params = qb.build_update()
+
+        response = await self.qm.write(query, params)
+
+        if not response.success:
+            raise from_status(
+                response.status_code, response.message, error_message=response.error
+            )
+
+        return ApiResponse.ok("Update role")
